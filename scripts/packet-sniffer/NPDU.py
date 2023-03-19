6d0b00ea4d7324f5389313547e8a28d26e88f8fc
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-03-20 02:30:44 +0530

    npdu

---
+++ scripts/packet-sniffer/NPDU.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# title: Network Protocol Data Units
+
+# imports
+import socket
+import struct
+from utils import *
+import time
+
+# Ethernet
+class Ethernet:
+    def __init__(self, raw_data):
+        dest, src, prototype = struct.unpack('! 6s 6s H', raw_data[:14])
+        self.dest_mac = get_mac_addr(dest)
+        self.src_mac = get_mac_addr(src)
+        self.proto = socket.htons(prototype)
+        self.data = raw_data[14:]
+
+# HTTPS
+class HTTP:
+    def __init__(self, raw_data):
+        try:
+            self.data = raw_data.decode('utf-8')
+        except:
+            self.data = raw_data
+
+# ICMP (Internet Control Message Protocol)
+class ICMP:
+    def __init__(self, raw_data):
+        self.type, self.code, self.checksum = struct.unpack('! B B H', raw_data[:4])
+        self.data = raw_data[4:]
+
+# IPv4
+class IPv4:
+    def __init__(self, raw_data):
+        version_header_length = raw_data[0]
+        self.version = version_header_length >> 4
+        self.header_length = (version_header_length & 15) * 4
+        self.ttl, self.proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', raw_data[:20])
+        self.src = self.ipv4(src)
+        self.target = self.ipv4(target)
+        self.data = raw_data[self.header_length:]
+    def ipv4(self, addr):
+        return '.'.join(map(str, addr))
+
+# pcap
+class Pcap:
+    def __init__(self, filename, link_type=1):
+        self.pcap_file = open(filename, 'wb')
+        self.pcap_file.write(struct.pack('@ I H H i I I I', 0xa1b2c3d4, 2, 4, 0, 0, 65535, link_type))
+    def write(self, data):
+        ts_sec, ts_usec = map(int, str(time.time()).split('.'))
+        length = len(data)
+        self.pcap_file.write(struct.pack('@ I I I I', ts_sec, ts_usec, length, length))
+        self.pcap_file.write(data)
+    def close(self):
+        self.pcap_file.close()
+
+# TCP
+class TCP:
+    def __init__(self, raw_data):
+        (self.src_port, self.dest_port, self.sequence, self.acknowledgment, offset_reserved_flags) = struct.unpack(
+            '! H H L L H', raw_data[:14])
+        offset = (offset_reserved_flags >> 12) * 4
+        self.flag_urg = (offset_reserved_flags & 32) >> 5
+        self.flag_ack = (offset_reserved_flags & 16) >> 4
+        self.flag_psh = (offset_reserved_flags & 8) >> 3
+        self.flag_rst = (offset_reserved_flags & 4) >> 2
+        self.flag_syn = (offset_reserved_flags & 2) >> 1
+        self.flag_fin = offset_reserved_flags & 1
+        self.data = raw_data[offset:]
+
+# UDP
+class UDP:
+    def __init__(self, raw_data):
+        self.src_port, self.dest_port, self.size = struct.unpack('! H H 2x H', raw_data[:8])
+        self.data = raw_data[8:]
+
